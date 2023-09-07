133c7be5b56da2df34f3bc7ca72a6c8878052d40
Gowtam Gottam <gg694@nau.edu>
2023-09-07 02:17:21 -0400

    deployed webs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import { Container, Divider, Grid, Text, Box, HStack, Heading, Center, Stack } f
 import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
 import Lottie from "lottie-react";
 import animationData from "../assets/VH2hQtGFTw.json";
-
+import { Reveal } from "./Reveal.tsx";
 export default function Contact({ color }) {
   return (
     <Container maxW={"8xl"} id="contact">
@@ -19,10 +19,13 @@ export default function Contact({ color }) {
           <Divider orientation="horizontal" mb={20} />
           <Stack spacing={4} as={Container} maxW={"3xl"} textAlign={"center"}>
             <Heading fontSize={"3xl"}>Let's stay in touch!</Heading>
+            <Reveal>
             <Text color={"gray.600"} fontSize={"xl"} px={4}>
               Interested in software development and web development? Feel free to get in touch with me today!
             </Text>
+            </Reveal>
             <Center>
+            <Reveal>
               <HStack spacing={8} pt={4}>
                 {/* LinkedIn */}
                 <a href="https://www.linkedin.com/in/gowtam-1998/" target="_blank" rel="noopener noreferrer">
@@ -40,14 +43,17 @@ export default function Contact({ color }) {
                   <FaPhone size={44} color={`${color}.500`} />
                 </a>
               </HStack>
+              </Reveal>
             </Center>
             <Center>
               {/* Email */}
+              <Reveal>
               <Text fontWeight={600} fontSize={"lg"} mt={10} color={"gray.600"}>
                 <a href="mailto:Gowtamssg@gmail.com">
                   Gowtamssg@gmail.com
                 </a>
               </Text>
+              </Reveal>
             </Center>
           </Stack>
         </Box>
